9a69d858c21d773d81d39c67b28fa015844f9400
Shaun O'Neill <oshauneill@hotmail.com>
2021-06-05 15:06:03 -0400

    testing

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
 
 let Person;
 
