ad02cde991fb09a8f7aad63fe3e02565fe542b31
Shaun O'Neill <oshauneill@hotmail.com>
2021-06-05 14:53:42 -0400

    connect to mongoose

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
 
+const mongoose = require('mongoose');
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
